8a51a4406dc5fac6ca0c77d5f99094c9aa4f3801
Luke_sinclair <Lukesin@outlook.com>
2025-11-13 09:22:56 +0100

    wokr

--- frontend-lagersystem/src/app/toolbar/admin/admin.ts
+++ frontend-lagersystem/src/app/toolbar/admin/admin.ts
@@ -1,19 +1,33 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth-service';
 import { NgIf } from '@angular/common';
 import { Subject, Subscription, takeUntil } from 'rxjs';
 import { AdminApiCallServices } from '../../../services/admin-api-call-services';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+
+export interface Product {
+  id: number;
+  navn: string;
+}
 
 @Component({
   selector: 'app-admin',
-  imports: [FormsModule, ReactiveFormsModule, NgIf],
+  imports: [FormsModule, ReactiveFormsModule, NgIf,MatTableModule, MatPaginatorModule,MatSortModule],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
 
 export class Admin implements OnInit, OnDestroy {
-CreateNewUser() {
+
+  displayedColumns: string[] = ['id', 'navn'];
+  dataSource = new MatTableDataSource<Product>([]);
+  products: Product[] = [{id:1,navn:'test'}];
+
+  CreateNewUser() {
   this.adminService.createUser(this.test.value).subscribe(()=>{
     this.test.reset();
   });
@@ -24,6 +38,7 @@ CreateNewUser() {
   test!: FormGroup;
   isLoggedIn: boolean = false;
   private destroy$ = new Subject<void>();
+  private _liveAnnouncer = inject(LiveAnnouncer);
   
   
   ngOnInit(): void {
@@ -49,7 +64,23 @@ CreateNewUser() {
       password: new FormControl('', Validators.required),
     });
   }
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
   constructor(
     private authService: AuthService,
     private adminService: AdminApiCallServices
